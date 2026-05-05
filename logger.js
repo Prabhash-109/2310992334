ddfd56d0539fb2ec55dfa62ed019866104d9b041
Prabhash-109 <prabhashjha92560@gmail.com>
2026-05-05 11:23:57 +0530

    Add client id and secret in .env

---
+++ logger.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+require("dotenv").config();
+
+let token = "";
+
+// 🔹 Get Token
+async function getToken() {
+    const res = await axios.post(
+        "http://20.207.122.201/evaluation-service/auth",
+        {
+            email: process.env.EMAIL,
+            name: process.env.NAME,
+            rollNo: process.env.ROLL_NO,
+            accessCode: process.env.ACCESS_CODE,
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET
+        }
+    );
+
+    token = res.data.access_token;
+}
+
+// 🔹 MAIN LOG FUNCTION
+async function Log(stack, level, pkg, message) {
+    try {
+        if (!token) {
+            await getToken();
+        }
+
+        const res = await axios.post(
+            "http://20.207.122.201/evaluation-service/logs",
+            {
+                stack: stack,
+                level: level,
+                package: pkg,
+                message: message
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        );
+
+        console.log("Log Sent:", res.data.message);
+    } catch (err) {
+        console.error("Log Error:", err.message);
+    }
+}
+
+module.exports = Log;
